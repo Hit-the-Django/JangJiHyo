90687d90d40d11c5bedbe120ef1658e9bc107cde
jihyo <wendyjihyo77@sookmyung.ac.kr>
2023-11-20 22:59:15 +0900

    8주차 과제

--- WEEK06/posts/views.py
+++ WEEK06/posts/views.py
@@ -3,7 +3,62 @@ from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from .forms import PostBasedForm, PostCreateForm, PostUpdateForm, PostDetailForm
-from .models import Post
+from .models import Post, Posts
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostModelSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class PostModelViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostModelSerializer
+ 
+class PostList(APIView):
+    # Blog list를 보여줄 때
+    def get(self, request):
+        queryset = Post.objects.all()
+        # 여러 개의 객체를 serialization하기 위해 many=True로 설정
+        serializer = PostModelSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # 새로운 Blog 글을 작성할 때
+    def post(self, request):
+        # request.data는 사용자의 입력 데이터
+        serializer = PostModelSerializer(data=request.data)
+        if serializer.is_valid(): #유효성 검사
+            serializer.save() # 저장
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Blog의 detail을 보여주는 역할
+class PostDetail(APIView):
+    # Blog 객체 가져오기
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+    
+    # Blog의 detail 보기
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostModelSerializer(post)
+        return Response(serializer.data)
+
+    # Blog 수정하기
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostModelSerializer(post, data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Blog 삭제하기
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
 
 def index(request):
     post_list= Post.objects.all().order_by('-created_at')
@@ -75,7 +130,7 @@ def post_update_view(request, id):
         return render(request, 'posts/post_form.html', context)
     elif request.method == 'POST':
         new_image = request.FILES.get('image')
-        content = rerquest.POST.get('content')
+        content = request.POST.get('content')
         print(new_image)
         print(content)
 
@@ -97,6 +152,7 @@ def post_delete_view(request, id):
     if request.method == 'GET':
         context={ 'post': post }
         return render(request, 'posts/post_confirm_delete.html')
+
     else:
         post.delete()
         return redirect('index')
